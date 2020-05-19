9c5fd109a3188fd2f6859d9af4fc93b70f764a12
a2 <a2@localhost.localdomain>
2020-05-19 16:53:18 -0300

    Iniciar repositório com projeto Flight Facilities

---
+++ src/main/java/com/br/FlightFacilities/FlightFacilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.br.FlightFacilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFacilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFacilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/FlightFacilities/FlightFacilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.FlightFacilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFacilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
